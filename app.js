bd9c309210d28c9175d70938616a816e9ce31ae8
Filipe Ferreira <filipe.ferreira@sxgrupo.com>
2023-08-01 11:39:55 -0300

    vetor+rolagem

--- app.js
+++ app.js
@@ -1,58 +1,89 @@
 function Inserir() {
-    let musicnames = ["Blinding Lights", "Love of My life","Old Town road", "Creepin"];
-    let artistnames = ["The weekend", "Queen","Lil NasX", "The Weekend"];
-    const div = document.querySelector('.playlist-menu');
-    for (i = 0; i < 5; i++) {
-        div.innerHTML += 
-        `<div class="playlists">
+  let musics = {
+    info: [
+      {
+        nome: "Blinding Lights",
+        artista: "The Weekend"
+      },
+      {
+        nome: "Take on me",
+        artista: "A-ha"
+      },
+      {
+        nome: "Dont stop me now",
+        artista: "Queen"
+      },
+      {
+        nome: "Circles",
+        artista: "Post Malone"
+      },
+      {
+        nome: "Is there someone else?",
+        artista: "The Weekend"
+      },
+      {
+        nome: "Every Breath you take",
+        artista: "The police"
+      },
+      {
+        nome: "I will find",
+        artista: "Vintage Culture"
+      },
+    ]
+  };
+
+  const div = document.querySelector('.playlist-menu');
+  for (i = 0; i < 5; i++) {
+    div.innerHTML +=
+      `<div class="playlists">
             <img src="icon-liked.jpg" class="icon">
             <p>Liked Songs</p>
         </div>`
-    }
-    const div1 = document.querySelector('.menu-2');
-    for (i = 0; i < 4; i++) {
-        div1.innerHTML += 
-        `<div class="might-like">
+  }
+  const div1 = document.querySelector('.menu-2');
+  for (i = 0; i < musics.info.length; i++) {
+    div1.innerHTML +=
+      `<div class="might-like">
             <img src="icon-liked.jpg" class="artist">
             <div class="info-liked">
-                <h5 style="font-size: 19px;">${musicnames[i]}</h5>
-                <p style="color: #B3B3B3;">${artistnames[i]}</p>
+                <h5 style="font-size: 19px;">${musics.info[i].nome}</h5>
+                <p style="color: #B3B3B3;">${musics.info[i].artista}</p>
             </div>
         </div>`
-    }
+  }
 }
 Inserir();
 
-function AparecerDiv(){
-    const div = document.querySelector('.device');
-    div.style.display = 'block';
-    div.style.display = 'flex';
+function AparecerDiv() {
+  const div = document.querySelector('.device');
+  div.style.display = 'block';
+  div.style.display = 'flex';
 }
 
+//Volume bar
+document.addEventListener('DOMContentLoaded', function () {
+  const volumeBar = document.querySelector('.volume-bar');
+  const volumeFill = document.querySelector('.volume-fill');
 
-  document.addEventListener('DOMContentLoaded', function() {
-    const volumeBar = document.querySelector('.volume-bar');
-    const volumeFill = document.querySelector('.volume-fill');
+  volumeBar.addEventListener('click', function (event) {
+    const clickX = event.clientX - volumeBar.getBoundingClientRect().left;
+    const barWidth = volumeBar.offsetWidth;
+    const volume = clickX / barWidth;
 
-    volumeBar.addEventListener('click', function(event) {
-      const clickX = event.clientX - volumeBar.getBoundingClientRect().left;
-      const barWidth = volumeBar.offsetWidth;
-      const volume = clickX / barWidth;
-
-      volumeFill.style.width = (volume * 100) + '%';
-    });
+    volumeFill.style.width = (volume * 100) + '%';
   });
-  document.addEventListener('DOMContentLoaded', function() {
-    const volumeBar = document.querySelector('.volume-bar1');
-    const volumeFill = document.querySelector('.volume-fill1');
+});
+document.addEventListener('DOMContentLoaded', function () {
+  const volumeBar = document.querySelector('.volume-bar1');
+  const volumeFill = document.querySelector('.volume-fill1');
 
-    volumeBar.addEventListener('click', function(event) {
-      const clickX = event.clientX - volumeBar.getBoundingClientRect().left;
-      const barWidth = volumeBar.offsetWidth;
-      const volume = clickX / barWidth;
+  volumeBar.addEventListener('click', function (event) {
+    const clickX = event.clientX - volumeBar.getBoundingClientRect().left;
+    const barWidth = volumeBar.offsetWidth;
+    const volume = clickX / barWidth;
 
-      volumeFill.style.width = (volume * 100) + '%';
-    });
+    volumeFill.style.width = (volume * 100) + '%';
   });
+});
 
-   
+
